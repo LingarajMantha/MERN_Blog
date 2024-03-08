4e305ad97a8b10736e494f350e00c5fb09ab309b
LingarajMantha <lingarajmantha10@gmail.com>
2024-03-09 01:18:39 +0530

    craete pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import home from './Pages/home'
+import about, { ABout } from './Pages/ABout'
+import signin from './Pages/signin'
+import signup from './Pages/signup'
+import dashboard from './Pages/dashboard'
+import projects from './Pages/projects'
 
-export default function App() {
+
+
+
+export const App = () => {
   return (
-    <div className='text-3xl text-red-600'>App</div>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<home/>} />
+        <Route path='/about' element={<ABout/>} />
+        <Route path='/signin' element={<signin/>} />
+        <Route path='/signup' element={<signup/>} />
+        <Route path='/dashboard' element={<dashboard/>} />
+        <Route path='/projects' element={<projects/>} />
+
+
+    </Routes>
+    </BrowserRouter>
   )
 }
